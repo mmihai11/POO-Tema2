3e758b99bef5727cbd4e06746a53d24bea6ac8e5
eugenmihai17 <35217605+eugenmihai17@users.noreply.github.com>
2018-01-15 11:11:27 +0200

    modificafunctie.cpp
    Comentariul nu este adecvat ! Change it !

--- functia4.1.cpp
+++ functia4.1.cpp
@@ -1,6 +1,6 @@
 double getHammingDistance(vector <bool> v1, vector <bool> v2)
 {
-    // am adaugat un comm.
+    // am adaugat un comm // 
     int hammingDistanceValue = 0;
     for(int i = 0; i < v1.size(); i++)
         if(v1[i] != v2[i])
