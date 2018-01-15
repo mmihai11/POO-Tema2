330339dfd0064424042bb44c54a606450baad0dc
alexmunteanu97 <35217623+alexmunteanu97@users.noreply.github.com>
2018-01-15 10:46:28 +0200

    Update functia3.2.cpp

--- functia3.2.cpp
+++ functia3.2.cpp
@@ -1,20 +1,3 @@
-ofstream out("date.out");
-void getHashBit(CImg <unsigned char> image, vector <bool> &hashbit)
-{
-    vector <double> colors, avg;
-    double averageValue = 0;
-
-    double w = image.width(), h = image.height(), sum = 0;
-
-    for(int i = 0; i < w; i++)
-    {
-        sum = 0;
-        for(int j = 0; j < h; j++)
-            sum += colorPixel(image,i,j);
-        colors.push_back(sum);
-        avg.push_back(sum/h*1.0);
-    }
-
     for(int i = 0; i < avg.size(); i++)
         averageValue += avg[i];
     averageValue = averageValue/avg.size();
