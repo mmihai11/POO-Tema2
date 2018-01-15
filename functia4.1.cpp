3335054b86c8c3835293e3abc7630ce4d2a38a9b
andreidica <35217312+andreidica@users.noreply.github.com>
2018-01-15 11:20:23 +0200

    Update functia4.1.cpp

--- functia4.1.cpp
+++ functia4.1.cpp
@@ -4,6 +4,9 @@ double getHammingDistance(vector <bool> v1, vector <bool> v2)
     int hammingDistanceValue = 0;
     for(int i = 0; i < v1.size(); i++)
         if(v1[i] != v2[i])
+           
+            //dcasfsafsa
+            //fsafsafafsa
             hammingDistanceValue++;
     return (double)((double)hammingDistanceValue/v1.size());
 }
